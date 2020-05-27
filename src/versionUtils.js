c08bf4562bdc1a502955a229381288e6d7c512e3
Swapnil Devesh <sid.swapnildevesh@gmail.com>
2020-05-28 04:47:08 +0530

    Fix lint issues

--- src/versionUtils.js
+++ src/versionUtils.js
@@ -1,14 +1,14 @@
-export const isNumber = str => (parseInt(str, 10).toString() == str) && (str.indexOf('-') === -1);
+export const isNumber = str => (parseInt(str, 10).toString() === str) && (str.indexOf('-') === -1);
 
 export const isVersionFormatCorrect = (versionString) => {
   if (versionString.split('+').length !== 2) return false;
-  const [ versionName, versionCode ] = versionString.split('+');
+  const [versionName, versionCode] = versionString.split('+');
   if (versionName.split('.').length !== 3) return false;
-  const [ major, minor, patch ] = versionName.split('.');
+  const [major, minor, patch] = versionName.split('.');
   return (
-    isNumber(major) &&
-    isNumber(minor) &&
-    isNumber(patch) &&
-    isNumber(versionCode)
+    isNumber(major)
+    && isNumber(minor)
+    && isNumber(patch)
+    && isNumber(versionCode)
   );
-}
+};
